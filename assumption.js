54f08f62fa767409370614f76ed876692b02574e
James Jansson <jamesjansson@gmail.com>
2015-08-25 23:16:39 +1000

    First play with structure

---
+++ assumption.js
@@ -0,0 +1,42 @@
+function Assumption(ArrayOfOutcomes, TruthDecision, ArrayOfConditions, Domains){
+	// Note that the variable names must be consistent when specifying the assumtion
+	
+	// TruthDecision = true, false, possibly true, true under unknown conditions, 
+
+	// Array of conditions is empty if there are zero conditions
+	
+	// Domains
+		// Natural
+		// Integer
+		// Real
+		// Rational
+		// Irrational
+		// Complex
+		// Assumption
+		// Functions
+		// Equation/inequality
+		// Positive/negative
+	// Domains=[];
+	// Domains[0]={};
+	// Domains[0].Variable="x";
+	// Domains[0].Domains=["N", "R", "C"];
+	
+	
+
+
+}
+
+
+
+
+
+Assumption.prototype.Substitute=function(){
+	
+	
+};
+
+
+//list of inititating values
+// x=y true if y=x
+// x+b=y+b true if x=y
+// x<y false if x>y
---
+++ expression.js
@@ -0,0 +1,49 @@
+// expression
+
+function Expression(ExpressionText, SymbolProperties, Name, Description){
+	this.Name=Name;
+	this.Desecription=Description;
+	this.ExpressionText=ExpressionText;// a*(b+c)=a*b+a*c, a*b=b*a, 
+	
+	this.VariableName=[];//[0]="a"
+	
+	this.NumerateSymbols(InputExpression, SymbolProperties);
+	
+}
+
+Expression.prototype.NumerateSymbols=function(InputExpression, SymbolProperties){
+	// Find the variable positions in the expression
+	
+	// 	
+}
+
+Expression.prototype.Substitute=function(variableName, InputExpression){
+	this.Match(InputExpression);
+	
+	A=new Expression(undefined, undefined, );
+	
+}
+
+Expression.prototype.Match=function(InputExpression){
+	// find sub-expressions that match the structure of the input expression
+}
+
+Expression.prototype.Split=function(){
+	
+}
+
+Expression.prototype.SubExpression=function(InputExpression, ){
+	// find elements that can be considered whole under the order of operations
+	
+	 
+}
+
+Expression.prototype.Isolate=function(SpecificVariableName){
+	
+}
+
+
+function FunctionalExpression(){
+	
+	
+}
